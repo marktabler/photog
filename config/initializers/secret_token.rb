430ebd1ff1a3652e7edc10ef3c8839c4de0c0364
Mark Tabler <mtabler@newrelic.com>
2013-11-24 14:04:14 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Photog::Application.config.secret_key_base = '78a25cb83eb95f326ca3d33aad1167525a5bf90e49656135400e717f83e29985f55b9258104eb3b4e2e21faffdeec7edf700c6cd488f86250152a4bc3299bbcd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Photog::Application.config.session_store :cookie_store, key: '_photog_session'
