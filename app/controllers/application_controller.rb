77ebdd735e6b01f70ce980fb958d732818115980
Mark Tabler <mtabler@newrelic.com>
2013-11-24 19:16:54 -0800

    Add admin gate to photo editing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,8 @@ class ApplicationController < ActionController::Base
   def current_user
     User.find(session[:user_id]) if session[:user_id]
   end
+
+  def admin_required
+    return redirect_to root_path unless current_user && current_user.admin
+  end
 end
