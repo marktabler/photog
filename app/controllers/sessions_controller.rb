1b3d57715a97d3a906ecfa28203385644b3f683b
Mark Tabler <mtabler@newrelic.com>
2013-11-24 19:02:57 -0800

    Bugfix tweaks

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,12 @@
 class SessionsController < ApplicationController  
   def create
     @user = User.lookup(auth_hash)
-    self.current_user = @user
+    session[:user_id] = @user.id
+    redirect_to '/'
+  end
+
+  def destroy
+    session[:user_id] = nil
     redirect_to '/'
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Photog::Application.routes.draw do
   resources :photos
   resources :albums
   get '/auth/:provider/callback', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
