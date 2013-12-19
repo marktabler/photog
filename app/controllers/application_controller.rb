fbdef8f4cf6e08c76f05e74a3c67e9da5b46cbea
Mark Tabler <mtabler@newrelic.com>
2013-12-19 00:53:14 -0800

    Add backtrace info to all 500 errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  rescue_from Exception, :with => :log_error
 
   def current_user
     User.find(session[:user_id]) if session[:user_id]
@@ -21,5 +22,11 @@ class ApplicationController < ActionController::Base
     if Rails.env.production?
       return redirect_to root_path unless current_user && current_user.admin
     end
+  end
+
+  def log_error(e)
+    Rails::logger.error(e.inspect)
+    Rails::logger.error(e.backtrace.join("\n"))
+    raise e
   end
 end
